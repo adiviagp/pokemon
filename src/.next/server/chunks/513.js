654a42558d0a61f317224357fd05b1a503a9083b
Adivia Gilang Prakarsa <adivia.gilang@onebyone.com.sg>
2022-03-30 08:27:48 +0700

    build: add .next folder

---
+++ src/.next/server/chunks/513.js
@@ -0,0 +1,190 @@
+"use strict";
+exports.id = 513;
+exports.ids = [513];
+exports.modules = {
+
+/***/ 2562:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/loaderbg.6b56bab7.png","height":100,"width":100,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAQAAABuBnYAAAAAkklEQVR42mOIY65gWCFcHV0VN0+qiMGVmaGCodsx/3Xq/9T/uZ/a/QsYGBZI5P1K+p9/t+B60v/c/5PVGepd067krK2bV7sof3nqtZowhnqXtJO5C2oWV68qmJVypjqEYaZ87v/k/3mX888m/8/532/AUMTQ7p/7HWRozt+W+AwGBhfmAobpShUF5cWTNDMYGJgAVBk9OkmcO0wAAAAASUVORK5CYII="});
+
+/***/ }),
+
+/***/ 8992:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1480);
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_emotion_styled__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+const ContainerWrap = (_emotion_styled__WEBPACK_IMPORTED_MODULE_1___default().div)`
+  max-width: 500px;
+  background-color: #fafafa;
+  margin: 0px auto;
+  padding: ${(props)=>props.noPadding ? '0' : '2em'
+};
+  min-height: 100vh;
+  overflow: hidden;
+`;
+const Container = (props)=>{
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(ContainerWrap, {
+        ...props,
+        children: props.children
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Container);
+
+
+/***/ }),
+
+/***/ 4506:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1480);
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_emotion_styled__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _public_loaderbg_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(2562);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(8579);
+
+
+
+
+
+const PokeLoader = (_emotion_styled__WEBPACK_IMPORTED_MODULE_1___default().div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+`;
+const Spinner = (_emotion_styled__WEBPACK_IMPORTED_MODULE_1___default().span)`
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+
+  @-webkit-keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+const Loader = ()=>{
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(PokeLoader, {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Spinner, {
+            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_image__WEBPACK_IMPORTED_MODULE_4__["default"], {
+                src: _public_loaderbg_png__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z,
+                width: 80,
+                height: 80,
+                alt: "loader"
+            })
+        })
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Loader);
+
+
+/***/ }),
+
+/***/ 3585:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1480);
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_emotion_styled__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+const H1 = (_emotion_styled__WEBPACK_IMPORTED_MODULE_1___default().h1)`
+  font-size: 2em;
+  padding: 0;
+  margin: 0;
+  color: ${(props)=>props.color || 'black'
+};
+  z-index: 999;
+`;
+const H2 = (_emotion_styled__WEBPACK_IMPORTED_MODULE_1___default().h2)`
+  font-size: 1.25em;
+  padding: 0;
+  margin: 0;
+  color: ${(props)=>props.color || 'black'
+};
+  z-index: 999;
+`;
+const H3 = (_emotion_styled__WEBPACK_IMPORTED_MODULE_1___default().h3)`
+  font-size: 0.8em;
+  padding: 0;
+  margin: 0;
+  color: ${(props)=>props.color || 'black'
+};
+  z-index: 999;
+`;
+const P = (_emotion_styled__WEBPACK_IMPORTED_MODULE_1___default().p)`
+  font-size: 0.75em;
+  color: ${(props)=>props.color || 'black'
+};
+  z-index: 999;
+  position: relative;
+`;
+const Typography = ({ variant , color , children  })=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            variant === 'h1' && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(H1, {
+                color: color,
+                children: children
+            }),
+            variant === 'h2' && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(H2, {
+                color: color,
+                children: children
+            }),
+            variant === 'h3' && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(H3, {
+                color: color,
+                children: children
+            }),
+            variant === 'p' && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(P, {
+                color: color,
+                children: children
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Typography);
+
+
+/***/ })
+
+};
+;
---
+++ src/.next/server/chunks/690.js
@@ -0,0 +1,142 @@
+"use strict";
+exports.id = 690;
+exports.ids = [690];
+exports.modules = {
+
+/***/ 9690:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "_p": () => (/* binding */ addApolloState),
+  "in": () => (/* binding */ initializeApollo),
+  "Uk": () => (/* binding */ useApollo)
+});
+
+// UNUSED EXPORTS: APOLLO_STATE_PROP_NAME
+
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(6689);
+// EXTERNAL MODULE: external "@apollo/client"
+var client_ = __webpack_require__(9114);
+// EXTERNAL MODULE: ./lib/myPokemonVar.ts
+var myPokemonVar = __webpack_require__(9627);
+;// CONCATENATED MODULE: ./lib/apolloClient.ts
+
+
+function createApolloClient() {
+    return new client_.ApolloClient({
+        ssrMode: "undefined" === 'undefined',
+        link: new client_.HttpLink({
+            uri: 'https://graphql-pokeapi.graphcdn.app/',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true
+            }
+        }),
+        cache: new client_.InMemoryCache({
+            typePolicies: {
+                Query: {
+                    fields: {
+                        pokemons: {
+                            keyArgs: false,
+                            merge (existing, incoming) {
+                                if (!incoming) return existing;
+                                if (!existing) return incoming; // existing will be empty the first time
+                                const { results , ...rest } = incoming;
+                                let res = rest;
+                                res.results = [
+                                    ...existing.results,
+                                    ...results
+                                ]; // Merge existing items with the items from incoming
+                                return res;
+                            }
+                        },
+                        cartItems: {
+                            read () {
+                                return (0,myPokemonVar/* default */.Z)();
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    });
+}
+
+// EXTERNAL MODULE: external "deepmerge"
+var external_deepmerge_ = __webpack_require__(6330);
+var external_deepmerge_default = /*#__PURE__*/__webpack_require__.n(external_deepmerge_);
+// EXTERNAL MODULE: external "lodash/isEqual"
+var isEqual_ = __webpack_require__(113);
+var isEqual_default = /*#__PURE__*/__webpack_require__.n(isEqual_);
+;// CONCATENATED MODULE: ./lib/apollo.ts
+
+
+
+
+const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
+let apolloClient;
+function initializeApollo(initialState = null) {
+    const _apolloClient = apolloClient !== null && apolloClient !== void 0 ? apolloClient : createApolloClient();
+    if (initialState) {
+        const existingCache = _apolloClient.extract();
+        // Merge the existing cache into data passed from getStaticProps/getServerSideProps
+        const data = external_deepmerge_default()(initialState, existingCache, {
+            arrayMerge: (destinationArray, sourceArray)=>[
+                    ...sourceArray,
+                    ...destinationArray.filter((d)=>sourceArray.every((s)=>!isEqual_default()(d, s)
+                        )
+                    ), 
+                ]
+        });
+        // Restore the cache with the merged data
+        _apolloClient.cache.restore(data);
+    }
+    // For SSG and SSR always create a new Apollo Client
+    if (true) return _apolloClient;
+    // Create the Apollo Client once in the client
+    if (!apolloClient) apolloClient = _apolloClient;
+    return _apolloClient;
+}
+function addApolloState(client, pageProps) {
+    if (pageProps === null || pageProps === void 0 ? void 0 : pageProps.props) {
+        pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
+    }
+    return pageProps;
+}
+function useApollo(pageProps) {
+    const state = pageProps[APOLLO_STATE_PROP_NAME];
+    const store = (0,external_react_.useMemo)(()=>initializeApollo(state)
+    , [
+        state
+    ]);
+    return store;
+}
+
+
+/***/ }),
+
+/***/ 9627:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "W": () => (/* binding */ MYPOKEMON_STORAGE),
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _apollo_client__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9114);
+/* harmony import */ var _apollo_client__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_apollo_client__WEBPACK_IMPORTED_MODULE_0__);
+
+const MYPOKEMON_STORAGE = 'mypokemon';
+let storage;
+if (false) {}
+const MyPokemonVar = (0,_apollo_client__WEBPACK_IMPORTED_MODULE_0__.makeVar)(storage || []);
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyPokemonVar);
+
+
+/***/ })
+
+};
+;
